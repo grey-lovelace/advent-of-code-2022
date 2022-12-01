6fd95a399a62741676eb09a5eafd5df6c5804dcd
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-11-30 23:33:08 -0600

    Add first day

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -2,14 +2,24 @@ import java.io.File
 
 class Day1: Day {
     override val resourcePath = "day01"
-    override val expectedPart1Results = listOf(0L)
-    override val expectedPart2Results = listOf(0L)
+    override val expectedPart1Results = listOf(24000L)
+    override val expectedPart2Results = listOf(45000L)
 
     override fun part1(file: File): Long {
-        return 0;
+        return getCaloriesPerElf(file).maxOrNull() as Long;
     }
 
     override fun part2(file: File): Long {
-        return 0;
+        return getCaloriesPerElf(file).sortedDescending().take(3).sum()
+    }
+
+    fun getCaloriesPerElf(file: File): MutableList<Long> {
+        return file.readLines().fold(mutableListOf(0L)) { acc, line -> 
+            when {
+                line.isNotBlank() -> acc[acc.lastIndex] += line.toLong()
+                else -> acc += 0
+            }
+            acc
+        }
     }
 }
