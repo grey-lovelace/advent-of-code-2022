61256521ceecb5c342b8921ec7b2cf8add490082
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-02 07:57:46 -0600

    Add better error message when tests fail

--- src/main/kotlin/Day.kt
+++ src/main/kotlin/Day.kt
@@ -10,14 +10,16 @@ interface Day {
 
         expectedPart1Results.forEachIndexed{ i, it ->
             val sampleFileSuffux = if (i==0) "" else i;
-            check(it == this.part1(File("${path}/sample${sampleFileSuffux}.txt")))
+            val result = this.part1(File("${path}/sample${sampleFileSuffux}.txt"))
+            check(it == result, { "Part 1 - sample${sampleFileSuffux}.txt Result: ${result}  Expected: ${it}" })
         }
         val part1Result = this.part1(File("${path}/input.txt"))
         println("Part1 = ${part1Result}")
 
         expectedPart2Results.forEachIndexed{ i, it ->
             val sampleFileSuffux = if (i==0) "" else i;
-            check(it == this.part2(File("${path}/sample${sampleFileSuffux}.txt")))
+            val result = this.part2(File("${path}/sample${sampleFileSuffux}.txt"))
+            check(it == result, { "Part 2 - sample${sampleFileSuffux}.txt Result: ${result}  Expected: ${it}" })
         }
         val part2Result = this.part2(File("${path}/input.txt"))
         println("Part2 = ${part2Result}")
