bb345612d540e2782a8f63478be40cf48f94e122
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-03 00:34:06 -0600

    Update day1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -2,20 +2,20 @@ import java.io.File
 
 class Day1: Day {
     override val resourcePath = "day01"
-    override val expectedPart1Results = listOf(24000L)
-    override val expectedPart2Results = listOf(45000L)
+    override val expectedPart1Results = listOf(24000)
+    override val expectedPart2Results = listOf(45000)
 
-    override fun part1(file: File): Long {
+    override fun part1(file: File): Int {
         return getCaloriesPerElf(file).maxOrNull()!!;
     }
 
-    override fun part2(file: File): Long {
+    override fun part2(file: File): Int {
         return getCaloriesPerElf(file).sortedDescending().take(3).sum()
     }
 
-    fun getCaloriesPerElf(file: File): List<Long> {
-        return file.readText().split(Regex("(?:\r?\n){2}")).map{
-            it.split(Regex("\r?\n")).map{ line -> line.toLong() }.sum()
-        }
+    fun getCaloriesPerElf(file: File): List<Int> {
+        return file.readText()
+            .split(Regex("(?:\r?\n){2}"))
+            .map{ it.split(Regex("\r?\n")).map(String::toInt).sum() }
     }
 }
