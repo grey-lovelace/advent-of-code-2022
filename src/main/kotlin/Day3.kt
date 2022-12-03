36010da6701f82d654047efb1067e857598d4af1
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-03 00:31:37 -0600

    day3

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -2,14 +2,24 @@ import java.io.File
 
 class Day3: Day {
     override val resourcePath = "day03"
-    override val expectedPart1Results = listOf(0L)
-    override val expectedPart2Results = listOf(0L)
+    override val expectedPart1Results = listOf(157)
+    override val expectedPart2Results = listOf(70)
 
-    override fun part1(file: File): Long {
-        return 0;
+    override fun part1(file: File): Int {
+        return file.readLines()
+            .map{ Pair(it.take(it.length/2), it.takeLast(it.length/2)) }
+            .map{ it.first.find(it.second::contains) }
+            .map( letters::indexOf )
+            .sum()
     }
 
-    override fun part2(file: File): Long {
-        return 0;
+    override fun part2(file: File): Int {
+        return file.readLines()
+            .windowed(3,3)
+            .map{ it[0].find{ l -> l in it[1] && l in it[2] } }
+            .map( letters::indexOf )
+            .sum()
     }
+
+    val letters = listOf(' ') + ('a'..'z') + ('A'..'Z')
 }
