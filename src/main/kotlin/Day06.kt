202b6ab1fa606c218c11415a920d7c79df7d0270
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-06 00:13:28 -0600

    Day 6

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -2,14 +2,23 @@ import java.io.File
 
 class Day06: Day {
     override val resourcePath = "day06"
-    override val expectedPart1Results = listOf(0L)
-    override val expectedPart2Results = listOf(0L)
+    override val expectedPart1Results = listOf(7)
+    override val expectedPart2Results = listOf(19)
 
-    override fun part1(file: File): Long {
-        return 0;
+    override fun part1(file: File): Int {
+        return findPacketMarker(file, 4);
     }
 
-    override fun part2(file: File): Long {
-        return 0;
+    override fun part2(file: File): Int {
+        return findPacketMarker(file, 14);
+    }
+
+    fun findPacketMarker(file: File, numOfUniqueChars: Int): Int {
+        return file.readText()
+            .withIndex()
+            .windowed(numOfUniqueChars)
+            .first{ window -> window.distinctBy{ it.value }.size == numOfUniqueChars }
+            .last()
+            .index + 1;
     }
 }
