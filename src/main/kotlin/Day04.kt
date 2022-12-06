d667ced33ec5a04a5faa63d2cd3fd7afcd600a10
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-06 00:26:09 -0600

    Use let

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -6,19 +6,21 @@ class Day04: Day {
     override val expectedPart2Results = listOf(4)
 
     override fun part1(file: File): Int {
-        return file.findRanges()
+        return file
+            .let(::findRanges)
             .filter{ (range1, range2) -> range1.all(range2::contains) || range2.all(range1::contains) }
             .count()
     }
 
     override fun part2(file: File): Int {
-        return file.findRanges()
+        return file
+            .let(::findRanges)
             .filter{ (range1, range2) -> range1.any(range2::contains) }
             .count()
     }
 
-    fun File.findRanges(): List<Pair<IntRange,IntRange>> {
-        return this.readLines()
+    fun findRanges(file: File): List<Pair<IntRange,IntRange>> {
+        return file.readLines()
             .map{ it.findNumbers() }
             .map{ Pair((it[0]..it[1]),(it[2]..it[3])) }
     }
