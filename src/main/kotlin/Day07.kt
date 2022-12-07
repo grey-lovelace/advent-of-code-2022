95e79e1d20e2bbcaae4dbd99c636d9ee1f246cf0
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-07 00:37:45 -0600

    Day7 rough

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -2,14 +2,34 @@ import java.io.File
 
 class Day07: Day {
     override val resourcePath = "day07"
-    override val expectedPart1Results = listOf(0L)
-    override val expectedPart2Results = listOf(0L)
+    override val expectedPart1Results = listOf(95437)
+    override val expectedPart2Results = listOf(24933642)
 
-    override fun part1(file: File): Long {
-        return 0;
+    override fun part1(file: File): Int {
+        return createSizeMap(file).values.filter{ it <= 100000 }.sum()
     }
 
-    override fun part2(file: File): Long {
-        return 0;
+    override fun part2(file: File): Int {
+        val sizeNeeded = 30000000 - (70000000 - file.readText().findNumbers().sum())
+        return createSizeMap(file).values.filter{ it >= sizeNeeded }.minOrNull()!!
+    }
+
+    fun createSizeMap(file: File): Map<List<String>,Int> {
+        var sizeMap = mutableMapOf<List<String>,Int>();
+        var currentDirStack = mutableListOf<String>();
+        file.readLines().forEach{
+            when {
+                it.equals("$ cd ..") -> currentDirStack.removeLast()
+                it.startsWith("$ cd") ->  currentDirStack.add(it.split(" ").last())
+                Regex("\\d+ .*") matches it -> {
+                    val fileSize = it.split(" ")[0].toInt()
+                    currentDirStack.indices.forEach{ i ->
+                        val dir = currentDirStack.slice(0..i)
+                        sizeMap.put(dir, sizeMap.getOrDefault(dir, 0) + fileSize);
+                    }
+                }
+            }
+        }
+        return sizeMap
     }
 }
