df1c174ff6f640cf3400ada10c974be6867060b1
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-07 20:31:34 -0600

    Make day7 more functional

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -6,30 +6,43 @@ class Day07: Day {
     override val expectedPart2Results = listOf(24933642)
 
     override fun part1(file: File): Int {
-        return createSizeMap(file).values.filter{ it <= 100000 }.sum()
+        return file
+            .let(::createSizeMap)
+            .values
+            .filter{ it <= 100000 }
+            .sum()
     }
 
     override fun part2(file: File): Int {
-        val sizeNeeded = 30000000 - (70000000 - file.readText().findNumbers().sum())
-        return createSizeMap(file).values.filter{ it >= sizeNeeded }.minOrNull()!!
+        return file
+            .let(::createSizeMap)
+            .values
+            .filter{ it >= 30000000 - (70000000 - file.readText().findNumbers().sum()) }
+            .minOrNull()!!
     }
 
     fun createSizeMap(file: File): Map<List<String>,Int> {
-        var sizeMap = mutableMapOf<List<String>,Int>();
-        var currentDirStack = mutableListOf<String>();
-        file.readLines().forEach{
-            when {
-                it.equals("$ cd ..") -> currentDirStack.removeLast()
-                it.startsWith("$ cd") ->  currentDirStack.add(it.split(" ").last())
-                Regex("\\d+ .*") matches it -> {
-                    val fileSize = it.split(" ")[0].toInt()
-                    currentDirStack.indices.forEach{ i ->
-                        val dir = currentDirStack.slice(0..i)
-                        sizeMap.put(dir, sizeMap.getOrDefault(dir, 0) + fileSize);
+        return file
+            .readLines()
+            .fold(Tracker(), { (currentDirStack, sizeMap), it ->
+                when {
+                    it.equals("$ cd ..") -> currentDirStack.removeLast()
+                    it.startsWith("$ cd") ->  currentDirStack.add(it.split(" ").last())
+                    Regex("\\d+ .*") matches it -> {
+                        val fileSize = it.split(" ")[0].toInt()
+                        currentDirStack.indices.forEach{ i ->
+                            val dir = currentDirStack.slice(0..i)
+                            sizeMap.put(dir, sizeMap.getOrDefault(dir, 0) + fileSize);
+                        }
                     }
                 }
-            }
-        }
-        return sizeMap
+                Tracker(currentDirStack, sizeMap)
+            })
+            .sizeMap
     }
+
+    data class Tracker (
+        val currentDirStack: MutableList<String> = mutableListOf<String>(),
+        var sizeMap: MutableMap<List<String>,Int> = mutableMapOf<List<String>,Int>()
+    )
 }
