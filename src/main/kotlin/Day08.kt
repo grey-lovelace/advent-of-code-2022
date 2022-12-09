492c024c776011cda961e95381d5d57d966b5a89
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-08 21:01:54 -0600

    remove this.

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -15,20 +15,20 @@ class Day08: Day {
 
     data class Matrix(val file: File) {
         val matrix = file.readLines().map{ line -> line.map{ it.toString().toInt() } }
-        val points = matrix.indices.flatMap{ x -> matrix[x].indices.map{ y -> Point(x, y, matrix[x][y], this.matrix) }}
+        val points = matrix.indices.flatMap{ x -> matrix[x].indices.map{ y -> Point(x, y, matrix[x][y], matrix) }}
     }
 
     data class Point(val x: Int, val y: Int, val value: Int, val matrix: List<List<Int>>) {
-        val left = (this.x-1 downTo 0).map{ matrix[it][this.y] }
-        val right = (this.x+1..matrix.lastIndex).map{ matrix[it][this.y] }
-        val above = (this.y-1 downTo 0).map{ matrix[this.x][it] }
-        val below = (this.y+1..matrix[0].lastIndex).map{ matrix[this.x][it] }
+        val left = (this.x-1 downTo 0).map{ matrix[it][y] }
+        val right = (x+1..matrix.lastIndex).map{ matrix[it][y] }
+        val above = (y-1 downTo 0).map{ matrix[x][it] }
+        val below = (y+1..matrix[0].lastIndex).map{ matrix[x][it] }
         val allAdjPoints = listOf(left, right, above, below)
 
-        fun isVisible(): Boolean = allAdjPoints.any{ adj -> adj.all{ this.value > it }}
+        fun isVisible(): Boolean = allAdjPoints.any{ adj -> adj.all{ value > it }}
 
         fun scenicScore(): Int = allAdjPoints.fold(1) { acc, adj ->
-            acc * adj.takeWhile{ this.value > it }.count()
+            acc * adj.takeWhile{ value > it }.count()
                 .let{ count -> (if (adj.isNotEmpty() && count < adj.size) 1 else 0) + count }
         }
     }
