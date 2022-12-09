4cbcca9cc36ce894d7a046c2143a6abe40458360
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-08 20:23:51 -0600

    move logic to Point in Day8

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -6,34 +6,37 @@ class Day08: Day {
     override val expectedPart2Results = listOf(8)
 
     override fun part1(file: File): Int {
-        return Matrix(file).let{m -> m.points.filter(m::isVisible) }.count()
+        return Matrix(file).points.filter(Point::isVisible).count()
     }
 
     override fun part2(file: File): Int {
-        return Matrix(file).let{m -> m.points.map(m::scenicScore) }.maxOrNull()!!
+        return Matrix(file).points.map(Point::scenicScore).maxOrNull()!!
     }
-
-    data class Point(val x: Int, val y: Int, val value: Int)
 
     data class Matrix(val file: File) {
         var cache = mutableMapOf<String, Point?>();
         val matrix = file.readLines().map{ line -> line.map{ it.toString().toInt() } }
-        val points = matrix.indices.flatMap{ x -> matrix[x].indices.map{ y -> Point(x, y, matrix[x][y]).also{ cache.put("$x~$y",it) } }}
+        val points = matrix.indices.flatMap{ x -> matrix[x].indices.map{ y -> Point(x, y, matrix[x][y], this).also{ cache.put("$x~$y",it) } }}
         fun findPointOrNull(x: Int, y: Int): Point? = cache.getOrPut("$x~$y") { points.firstOrNull{ it.x == x && it.y == y } }
-        fun leftOf(point: Point): List<Point> = (point.x-1 downTo 0).mapNotNull{ findPointOrNull(it, point.y) }
-        fun rightOf(point: Point): List<Point> = (point.x+1..matrix.lastIndex).mapNotNull{ findPointOrNull(it, point.y) }
-        fun above(point: Point): List<Point> = (point.y-1 downTo 0).mapNotNull{ findPointOrNull(point.x, it) }
-        fun below(point: Point): List<Point> = (point.y+1..matrix[0].lastIndex).mapNotNull{ findPointOrNull(point.x, it) }
+    }
 
-        fun isVisible(point: Point): Boolean {
-            return listOf(leftOf(point), rightOf(point), above(point), below(point))
-                .any{ adj -> adj.none{ point.value <= it.value }}
-        }
+    data class Point(val x: Int, val y: Int, val value: Int, val matrix: Matrix) {
+        val left = (this.x-1 downTo 0)
+        val right = (this.x+1..matrix.matrix.lastIndex)
+        val above = (this.y-1 downTo 0)
+        val below = (this.y+1..matrix.matrix[0].lastIndex)
 
-        fun scenicScore(point: Point): Int {
-            return listOf(leftOf(point), rightOf(point), above(point), below(point)).fold(1) { acc, adj ->
-                acc * adj.takeWhile{ point.value > it.value }.count().let{ count -> (if (adj.isNotEmpty() && count < adj.size) 1 else 0) + count }
-            }
+        fun allAdjPoints() = listOf(
+            left.mapNotNull{ matrix.findPointOrNull(it, this.y) },
+            right.mapNotNull{ matrix.findPointOrNull(it, this.y) },
+            above.mapNotNull{ matrix.findPointOrNull(this.x, it) },
+            below.mapNotNull{ matrix.findPointOrNull(this.x, it) })
+
+        fun isVisible(): Boolean = allAdjPoints().any{ adj -> adj.all{ this.value > it.value }}
+
+        fun scenicScore(): Int = allAdjPoints().fold(1) { acc, adj ->
+            acc * adj.takeWhile{ this.value > it.value }.count()
+                .let{ count -> (if (adj.isNotEmpty() && count < adj.size) 1 else 0) + count }
         }
     }
 }
