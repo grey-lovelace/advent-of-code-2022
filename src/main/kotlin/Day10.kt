55e0aa6f668a21f9715af286794e02b3f098dfe5
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-10 00:16:13 -0600

    Day 10 first pass

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -2,14 +2,44 @@ import java.io.File
 
 class Day10: Day {
     override val resourcePath = "day10"
-    override val expectedPart1Results = listOf(0L)
-    override val expectedPart2Results = listOf(0L)
+    override val expectedPart1Results = listOf(13140)
+    override val expectedPart2Results = listOf<String>()
 
-    override fun part1(file: File): Long {
-        return 0;
+    override fun part1(file: File): Int {
+        var x = 1
+        var cycle = 1
+        val importantCycles = (20..220 step 40)
+        var nums = 0
+        fun progress() {
+            if (importantCycles.contains(cycle)) nums += (x * cycle)
+            cycle++
+        }
+        file.readLines().map{ it.split(" ") }.forEach{ line ->
+            progress()
+            if (line.size == 2) {
+                progress()
+                x += line[1].toInt()
+            }
+        }
+        return nums;
     }
 
-    override fun part2(file: File): Long {
-        return 0;
+    override fun part2(file: File): String {
+        var x = 1
+        var cycle = 0
+        val importantCycles = (0..240 step 40)
+        var pixels = (0..239).map{' '}.toMutableList()
+        fun progress() {
+            if (importantCycles.flatMap{(it+x-1..it+x+1)}.contains(cycle)) pixels[cycle] = '#'
+            cycle++
+        }
+        file.readLines().map{ it.split(" ") }.forEach{ line ->
+            progress()
+            if (line.size == 2) {
+                progress()
+                x += line[1].toInt()
+            }
+        }
+        return "\n" + pixels.chunked(40).map{ it.joinToString("") }.joinToString("\n")
     }
 }
