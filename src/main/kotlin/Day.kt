89e7adb3e730689d4ad15360a0a656e753287aa4
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-14 00:55:50 -0600

    print out when tests pass

--- src/main/kotlin/Day.kt
+++ src/main/kotlin/Day.kt
@@ -11,7 +11,8 @@ interface Day {
         expectedPart1Results.forEachIndexed{ i, it ->
             val sampleFileSuffux = if (i==0) "" else i;
             val result = this.part1(File("${path}/sample${sampleFileSuffux}.txt"))
-            check(it == result, { "Part 1 - sample${sampleFileSuffux}.txt Result: ${result}  Expected: ${it}" })
+            println("Part1 - sample${sampleFileSuffux}.txt Result: ${result}  Expected: ${it}")
+            check(it == result)
         }
         val part1Result = this.part1(File("${path}/input.txt"))
         println("Part1 = ${part1Result}")
@@ -19,7 +20,8 @@ interface Day {
         expectedPart2Results.forEachIndexed{ i, it ->
             val sampleFileSuffux = if (i==0) "" else i;
             val result = this.part2(File("${path}/sample${sampleFileSuffux}.txt"))
-            check(it == result, { "Part 2 - sample${sampleFileSuffux}.txt Result: ${result}  Expected: ${it}" })
+            println("Part2 - sample${sampleFileSuffux}.txt Result: ${result}  Expected: ${it}")
+            check(it == result)
         }
         val part2Result = this.part2(File("${path}/input.txt"))
         println("Part2 = ${part2Result}")
