ce309efb87ed87483d5b63dfb75528b3efd2cef7
Grey Lovelace <austin.grey.lovelace@gmail.com>
2022-12-14 00:55:35 -0600

    Add org.json

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.json:json:20180130")
 }
 
 application {
